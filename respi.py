dbad184836c1cab376b9ed905d019556a37935fe
Wuyang Zheng <wuyangzheng@WuyangdeMini.lan>
2017-04-11 18:03:49 +0800

    py

--- respi.py
+++ respi.py
@@ -4,10 +4,7 @@ from lxml import html
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, sdch, br",
     "Accept-Language": "zh-CN,zh;q=0.8",
-    "Connection": "keep-alive",
-    "Host": "aax-us-east.amazon-adsystem.com",
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
     "Referer": "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=milk"
